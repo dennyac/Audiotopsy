18db04b361e7bd0a316a03b3935420d18d90d8d3
Denny Abraham Cheriyan <dennyac@gmail.com>
2014-07-02 21:43:31 -0700

    Corrected load_hbase_data script

--- bash/load_hbase_data.sh
+++ bash/load_hbase_data.sh
@@ -7,7 +7,7 @@ echo "Task YearWiseStats started execution at ${dt}" > ${LOG_PATH}/load_hbase_da
 java -cp $(hbase classpath):/home/ec2-user/denny/HBaseImport/target/HBaseImport-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dennyac.HBaseImport.YearWiseStats  > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1
 
 echo "Task TopSongsYearwise started execution at ${dt}" > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1
-java -cp $(hbase classpath):/home/ec2-user/denny/HBaseImport/targetHBaseImport-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dennyac.HBaseImport.TopSongsYearwise > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1 
+java -cp $(hbase classpath):/home/ec2-user/denny/HBaseImport/target/HBaseImport-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dennyac.HBaseImport.TopSongsYearwise > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1 
 
 echo "Task TopSongs started execution at ${dt}" > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1
 java -cp $(hbase classpath):/home/ec2-user/denny/HBaseImport/target/HBaseImport-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dennyac.HBaseImport.TopSongs > ${LOG_PATH}/load_hbase_data_${dt}.log 2>&1 
