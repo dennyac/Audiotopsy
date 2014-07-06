d3d96e09612681172cbe97249644912a293393cf
Denny Abraham Cheriyan <dennyac@gmail.com>
2014-07-05 21:24:42 -0700

    Updated audiotopsy webapp

---
+++ audiotopsy/src/main/java/com/dennyac/audiotopsy/HBaseConnection.java
@@ -0,0 +1,67 @@
+package com.dennyac.audiotopsy;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HConnection;
+import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.client.HTableInterface;
+
+
+public class HBaseConnection {
+
+	public static Configuration conf;
+	public static HConnection connection;
+	public static HashMap<String,String> colQual;
+	
+	public static void initializeColQualMappings(){
+		colQual = new HashMap<String,String>();
+		colQual.put("ht", "hotttnesss");
+		colQual.put("du", "duration");
+		colQual.put("tp", "tempo");
+		colQual.put("db", "loudness");
+		colQual.put("dn", "danceability");
+		colQual.put("eg", "energy");
+		colQual.put("an", "artist_name");
+		colQual.put("sn", "title");
+		colQual.put("al", "album");
+	}
+
+	public static void connectHbase() {
+
+		try {
+			conf = HBaseConfiguration.create();
+			conf.set("hbase.zookeeper.quorum", "172.31.13.66");
+			conf.set("hbase.zookeeper.property.clientPort", "2181");
+			connection = HConnectionManager.createConnection(conf);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getColName(String col) {
+		return colQual.get(col);
+	}
+
+	public static HTableInterface getTable(String tableName) throws IOException {
+		return connection.getTable(tableName);
+	}
+
+	public static void closeTable(HTableInterface htable) throws IOException {
+		htable.close();
+	}
+
+	public static void disconnectHbase() {
+		try {
+			connection.close();
+			conf.clear();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ audiotopsy/src/main/java/com/dennyac/audiotopsy/MSDViews.java
@@ -0,0 +1,246 @@
+package com.dennyac.audiotopsy;
+
+import java.util.Map.Entry;
+
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.PageFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+public class MSDViews {
+
+	public JSONObject getYearWiseStats() {
+		JSONObject mainObj = new JSONObject();
+
+		try {
+			ResultScanner scanner = null;
+			HTableInterface table = HBaseConnection.getTable("msd_year");
+			Scan scan = new Scan();
+			scanner = table.getScanner(scan);
+
+			JSONArray yearWiseJson = new JSONArray();
+			mainObj.put("year_wise_stats", yearWiseJson);
+
+			for (Result res : scanner) {
+
+				JSONObject yearJson = new JSONObject();
+				yearJson.put("year", Bytes.toString(res.getRow()));
+
+				for (Entry<byte[], byte[]> col : res.getFamilyMap(
+						Bytes.toBytes("info")).entrySet()) {
+					yearJson.put(HBaseConnection.getColName(Bytes.toString(col.getKey())),
+							Bytes.toString(col.getValue()));
+				}
+				yearWiseJson.put(yearJson);
+				
+			}
+			HBaseConnection.closeTable(table);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		
+		return mainObj;
+	}
+	
+	public JSONArray getYearWiseStatsGraph() {
+
+		JSONArray yearWiseHotttnesss = new JSONArray();
+		try {
+			ResultScanner scanner = null;
+			HTableInterface table = HBaseConnection.getTable("msd_year");
+			Scan scan = new Scan();
+			scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("ht"));
+			scanner = table.getScanner(scan);
+
+			
+			JSONObject hotJson = new JSONObject();
+			yearWiseHotttnesss.put(hotJson);
+			hotJson.put("key", "Hotttnesss");
+			hotJson.put("color", "#ff7f0e");
+			JSONArray dataPoints = new JSONArray();
+			hotJson.put("values",dataPoints);
+			for (Result res : scanner) {
+				JSONObject dataPoint = new JSONObject();
+				dataPoint.put("x",Bytes.toString(res.getRow()));
+				dataPoint.put("y",Double.parseDouble(Bytes.toString(res.value())));
+	
+				dataPoints.put(dataPoint);
+				
+			}
+			HBaseConnection.closeTable(table);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		
+		return yearWiseHotttnesss;
+	}
+
+	// Expects year to be a valid year. Returns top songs for the input year.
+	public JSONArray getYearTopTracks(String year) {
+		//JSONObject mainObj = new JSONObject();
+		JSONArray yearTopTracks = null;
+		Filter filter = new PageFilter(50);
+		
+
+		try {
+			ResultScanner scanner = null;
+			HTableInterface table = HBaseConnection.getTable("msd_hotttnesss_yearwise");
+			Scan scan = new Scan(Bytes.toBytes(year), Bytes.toBytes(Integer
+					.toString(Integer.parseInt(year) + 1)));
+			scan.setFilter(filter);
+			scanner = table.getScanner(scan);
+
+			yearTopTracks = new JSONArray();
+			//mainObj.put(year, yearTopTracks);
+
+			for (Result res : scanner) {
+
+				JSONObject songJson = new JSONObject();
+
+				for (Entry<byte[], byte[]> col : res.getFamilyMap(
+						Bytes.toBytes("info")).entrySet()) {
+					songJson.put(HBaseConnection.getColName(Bytes.toString(col.getKey())),
+							Bytes.toString(col.getValue()));
+				}
+				yearTopTracks.put(songJson);
+				
+			}
+			HBaseConnection.closeTable(table);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return yearTopTracks;
+	}
+	
+	// Expects year to be a valid year. Returns top songs for the input year.
+		public JSONObject getYearTopTracksk(String year, String key) {
+			JSONObject mainObj = new JSONObject();
+			byte[] lastKey = null;
+			JSONArray yearTopTracks = null;
+			Filter filter = new PageFilter(20);
+			
+
+			//Adding multiple filters
+			try {
+				ResultScanner scanner = null;
+				HTableInterface table = HBaseConnection.getTable("msd_hotttnesss_yearwise");
+				Scan scan = new Scan(Bytes.toBytes(year), Bytes.toBytes(Integer
+						.toString(Integer.parseInt(year) + 1)));
+				//See if it makes sense to send the below statement in the constructor
+				if(!key.isEmpty() && key != null){
+					scan.setStartRow(Bytes.toBytes(key));
+				}
+				scan.setFilter(filter);
+				scanner = table.getScanner(scan);
+
+				yearTopTracks = new JSONArray();
+				//mainObj.put(year, yearTopTracks);
+
+				for (Result res : scanner) {
+
+					JSONObject songJson = new JSONObject();
+
+					for (Entry<byte[], byte[]> col : res.getFamilyMap(
+							Bytes.toBytes("info")).entrySet()) {
+						songJson.put(HBaseConnection.getColName(Bytes.toString(col.getKey())),
+								Bytes.toString(col.getValue()));
+					}
+					lastKey = res.getRow();
+					yearTopTracks.put(songJson);
+					
+				}
+				HBaseConnection.closeTable(table);
+				mainObj.append("year_top_tracks", yearTopTracks);
+				mainObj.append("last_key",Bytes.toString(lastKey));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+		
+			return mainObj;
+		}
+	
+	public JSONArray getYearTopTracksp(String year) {
+		//JSONObject mainObj = new JSONObject();
+		JSONArray yearTopTracks = null;
+		int counter = 0;
+		JSONObject songJson = null;
+
+		try {
+			ResultScanner scanner = null;
+			HTableInterface table = HBaseConnection.getTable("msd_hotttnesss_yearwise");
+			Scan scan = new Scan(Bytes.toBytes(year), Bytes.toBytes(Integer
+					.toString(Integer.parseInt(year) + 1)));
+			scanner = table.getScanner(scan);
+
+			yearTopTracks = new JSONArray();
+			//mainObj.put(year, yearTopTracks);
+
+			for (Result res : scanner) {
+
+				songJson = new JSONObject();
+
+				for (Entry<byte[], byte[]> col : res.getFamilyMap(
+						Bytes.toBytes("info")).entrySet()) {
+					songJson.put(HBaseConnection.getColName(Bytes.toString(col.getKey())),
+							Bytes.toString(col.getValue()));
+
+				}
+				if(counter++ == 50){
+					break;
+				}
+				yearTopTracks.put(songJson);
+			}
+			
+			HBaseConnection.closeTable(table);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return yearTopTracks;
+	}
+	
+	// Expects year to be a valid year. Returns top songs for the input year.
+	public JSONObject TopTracks() {
+		JSONObject mainObj = new JSONObject();
+		Filter filter = new PageFilter(50);
+
+		try {
+			ResultScanner scanner = null;
+			HTableInterface table = HBaseConnection.getTable("msd_hotttnesss");
+			Scan scan = new Scan(Bytes.toBytes("hot0.00"), Bytes.toBytes("hot1.00"));
+			scan.setFilter(filter);
+			scanner = table.getScanner(scan);
+
+			JSONArray yearTopTracks = new JSONArray();
+			mainObj.put("top_tracks", yearTopTracks);
+
+			for (Result res : scanner) {
+
+				JSONObject songJson = new JSONObject();
+
+				for (Entry<byte[], byte[]> col : res.getFamilyMap(
+						Bytes.toBytes("info")).entrySet()) {
+					songJson.put(HBaseConnection.getColName(Bytes.toString(col.getKey())),
+							Bytes.toString(col.getValue()));
+				}
+				yearTopTracks.put(songJson);
+				HBaseConnection.closeTable(table);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return mainObj;
+	}
+
+}
